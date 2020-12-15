ed1bcbcd94578c5d5502e9f93c9d7e5b3c8f00b6
deese <dsargisyan@gmail.com>
2020-12-15 18:27:29 +0400

    Fixed wifi connect

--- src/wifi_connector.h
+++ src/wifi_connector.h
@@ -1,6 +1,7 @@
 #include <ESP8266WebServer.h>
 #include <DNSServer.h>
 #include <WiFiManager.h> 
+#include <Esp.h>
 
 #ifndef WIFI_INFO_H_
 #define WIFI_INFO_H_
@@ -8,13 +9,13 @@
 #include <ESP8266WiFi.h>
 
 void wifiConnect() {
-
+	
 	WiFiManager wifiManager;
-
-	wifiManager.autoConnect("HKThermostat");
 
 	WiFi.persistent(false);
 	WiFi.setAutoReconnect(true);
+
+	wifiManager.autoConnect("HKThermostat");
 
 	printf("WiFi connected! IP: %s", WiFi.localIP().toString().c_str());
 
